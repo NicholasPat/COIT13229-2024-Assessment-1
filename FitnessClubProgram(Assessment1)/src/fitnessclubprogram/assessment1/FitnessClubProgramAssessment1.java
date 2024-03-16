8860e7a367a3ef7e68b93cfe7b19f2389593bbc5
linke <linke@Link-Asus>
2024-03-16 20:04:35 +1000

    Initial setup - File not populated yet

---
+++ FitnessClubProgram(Assessment1)/src/fitnessclubprogram/assessment1/FitnessClubProgramAssessment1.java
@@ -0,0 +1,12 @@
+package fitnessclubprogram.assessment1;
+
+/**
+ * @author linke
+ */
+public class FitnessClubProgramAssessment1 {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
