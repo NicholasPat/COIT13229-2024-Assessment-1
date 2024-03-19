ea1f421ad00dbf8e05cd62d9d8e9b07496675cbe
linke <linke@Link-Asus>
2024-03-20 00:18:09 +1000

    Near full functionality. Timer was added and functions. Adding data to file as the data is created works. Need to comb through still and make sure everything works and comment explaining the code and what it's doing

--- FitnessClubProgram(Assessment1)/src/fitnessclubprogram/assessment1/TCPServer.java
+++ FitnessClubProgram(Assessment1)/src/fitnessclubprogram/assessment1/TCPServer.java
@@ -1,19 +1,31 @@
 package fitnessclubprogram.assessment1;
 
 import java.io.EOFException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.TimerTask;
 
 /**
  * @author linke
  */
-public class TCPServer {
+public class TCPServer {    
     //Will become main method in the final 
     //public static void 
     public static void serverMain() { 
+        int interval = 2000; //Print to file every 2 seconds
+        java.util.Timer tm = new java.util.Timer(); 
+        tm.schedule(new WriteObjectToFile(), interval, interval);
+        
         try { 
             int serverPort = 8864 ; //Change port to required in thing 
             ServerSocket listenSocket = new ServerSocket(serverPort) ; 
@@ -25,13 +37,26 @@ public class TCPServer {
             System.out.println("Listen socket: " + e.getMessage()) ; 
         }
     }
+    
+    //This makes the .txt file from the entries given which is to be made into an object via createFile() method 
+    public static void updateTextFile(Member member) { 
+        try { 
+            FileWriter fw = new FileWriter("memberlist.txt", true) ; 
+            PrintWriter pw = new PrintWriter(fw) ; 
+            pw.write(member.getFirstName() + "--__--" + member.getLastName() + "--__--" + member.getAddress() + "--__--" + member.getPhoneNumber() + "\n") ; 
+            pw.close() ; 
+            fw.close() ; 
+        } catch (IOException e) { 
+            System.out.println("Members file doesn't exist") ; 
+        }
+    }
 }
 
 class Connection extends Thread { 
     ObjectInputStream in ; 
     ObjectOutputStream out ; 
     Socket clientSocket ; 
-    int count = 0 ; 
+    int count = 1 ; 
     
     public Connection(Socket aClientSocket) { 
         try { 
@@ -50,12 +75,81 @@ class Connection extends Thread {
             System.out.println("Receiving data from client: " + (count++)) ; 
             System.out.println(member.toString()) ;
             //Server output to client 
+            TCPServer.updateTextFile(member) ; 
             
-        }catch (EOFException e){System.out.println("EOF:"+e.getMessage());
+        } catch(EOFException e){System.out.println("EOF:"+e.getMessage());
 	} catch(IOException e) {System.out.println("readline:"+e.getMessage());
 	} catch(ClassNotFoundException ex){
             ex.printStackTrace();
 	}finally{ try {clientSocket.close();}catch (IOException e){/*close failed*/}}
     }
-    
 }
+
+class WriteObjectToFile extends TimerTask { 
+    @Override
+    public void run() { 
+        String fileName = "memberlistObject" ; 
+        ArrayList<Member> completeMemberList = readTextFile() ;  
+        FileOutputStream fos ; ObjectOutputStream out ; 
+        try { 
+            fos = new FileOutputStream(fileName) ;  
+            out = new ObjectOutputStream(fos) ; 
+            out.writeObject(completeMemberList) ; 
+            out.close() ; 
+            System.out.println("Member object persisted") ; 
+            completeMemberList.clear() ; 
+        } catch (IOException ex) { 
+            ex.printStackTrace() ; 
+        }
+    }
+    public static ArrayList<Member> readTextFile() { 
+        File file = new File("memberlist.txt") ; 
+        ArrayList<Member> fullMemberList = new ArrayList<>() ; 
+        try { 
+            Scanner fileInput = new Scanner(file) ; 
+            int i = 0 ; 
+            while (fileInput.hasNextLine()) { 
+                String stringText = fileInput.nextLine() ; 
+                String[] brokenString = stringText.split("--__--") ; 
+                
+                String firstName = brokenString[0] ; 
+                String lastName = brokenString[1] ; 
+                String address = brokenString[2] ; 
+                String phoneNumber = brokenString[3] ; 
+                
+                //Create member and add to the array 
+                fullMemberList.add(new Member(firstName, lastName, address, phoneNumber)) ; 
+                //System.out.println(fullMemberList.get(i).toString()) ; 
+                i++ ; 
+            }
+        } catch (FileNotFoundException e) { 
+            System.out.println("Failed readTextFile") ;
+        }
+        return fullMemberList ; 
+    }
+}
+
+
+
+
+/** 
+ *  //To be read on startup and initialised into an array. Is this even needed here 
+    //This is serialisation, so need to pick up the .txt file and convert to object 
+    private static ArrayList<Member> readFile() { 
+        String filename = "Members" ; 
+        ArrayList<Member> members = null ; 
+        FileInputStream fis ; 
+        ObjectInputStream in ; 
+        try { 
+            fis = new FileInputStream(filename) ; 
+            in = new ObjectInputStream(fis) ; 
+            members = (ArrayList<Member>)in.readObject() ; 
+            in.close() ; 
+        }catch (IOException ex) {ex.printStackTrace();
+        }catch (ClassNotFoundException ex) { ex.printStackTrace();} 
+        if (members == null) { 
+            return new ArrayList<>() ; 
+        }
+        return members ; 
+    }
+ */
