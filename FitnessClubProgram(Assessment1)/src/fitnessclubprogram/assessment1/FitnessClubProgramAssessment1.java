36d041737551f4f020436a37bafb63c903ce8f3e
linke <linke@Link-Asus>
2024-03-19 21:36:13 +1000

    Continuing to add to this main class. Will be adapted as the .jar files are made for each individual component

--- FitnessClubProgram(Assessment1)/src/fitnessclubprogram/assessment1/FitnessClubProgramAssessment1.java
+++ FitnessClubProgram(Assessment1)/src/fitnessclubprogram/assessment1/FitnessClubProgramAssessment1.java
@@ -6,23 +6,25 @@ import java.util.Scanner;
  * @author linke
  */
 public class FitnessClubProgramAssessment1 {
-    TCPClient tcpClient = new TCPClient() ; 
-    TCPServer tcpServer = new TCPServer() ; 
-    UDPClient udpClient = new UDPClient() ; 
-    UDPServer udpServer = new UDPServer() ; 
-    Member members = new Member() ; 
+    static TCPClient tcpClient = new TCPClient() ; 
+    static TCPServer tcpServer = new TCPServer() ; 
+    static UDPClient udpClient = new UDPClient() ; 
+    static UDPServer udpServer = new UDPServer() ; 
+    //Member members = new Member() ; 
     
     public static void main(String[] args) {
         //What I am doing here, for the beginning at least is for testing choose the testing parameters via this one program 
-        //The actual program will handle this differently but making it hacky atm 
+        //The actual program will handle this differently but making it hacky atm. I think it has to be different .jar -- 
+        //files 
         
         String instanceForTest = "0" ; 
         boolean state = true ; 
         //0 = Nothing, 1 = TCP Client, 2 = TCP Server, 3 = UDP Client, 4 = UDP Server 
         while (state == true) { 
             Scanner input = new Scanner(System.in) ; 
-            System.out.println("Please input the testing parameter desired, input = INT (1,4)" ) ; 
+            System.out.print("Please input the testing parameter desired, input = INT (1,4): " ) ; 
             instanceForTest = input.nextLine() ; 
+            System.out.println() ; 
             if (instanceForTest.equals("1") || (instanceForTest.equals("2")) || (instanceForTest.equals("3")) || (instanceForTest.equals("4")) ){ 
                 state = false ; 
             }
@@ -33,11 +35,12 @@ public class FitnessClubProgramAssessment1 {
                 //TCP Client 
                 //Customer front end that gathers the information for member(s) and send it to the TCP server for adding to persistance 
                 
+                tcpClient.socketConnection() ; 
                 break;
             case "2":
                 //TCP Server 
                 //Receives request from TCP Client(s) and adds their entries to the persisting file 
-                
+                tcpServer.serverMain() ; 
                 break;
             case "3":
                 //UDP Client 
@@ -55,36 +58,4 @@ public class FitnessClubProgramAssessment1 {
         }
         
     }
-    
-    private Member getMemberDetails() { 
-        Member tempMembers ; 
-        //First name, Last name, address, phone number. All Strings 
-        String firstName = null ; String lastName = null ; String address = null ; String phoneNumber = null ; 
-        
-        Scanner fNameInput = new Scanner(System.in) ; 
-        System.out.println("Enter users first name: ") ; 
-        firstName = fNameInput.nextLine() ; 
-        
-        System.out.println() ; 
-        
-        Scanner lNameInput = new Scanner(System.in) ; 
-        System.out.println("") ; 
-        lastName = lNameInput.nextLine() ; 
-        
-        System.out.println() ; 
-        
-        Scanner addressInput = new Scanner(System.in) ; 
-        System.out.println("") ; 
-        address = addressInput.nextLine() ; 
-        
-        System.out.println() ; 
-        
-        Scanner phoneInput = new Scanner(System.in) ; 
-        System.out.println() ; 
-        phoneNumber = phoneInput.nextLine() ; 
-        
-        tempMembers = new Member(firstName, lastName, address, phoneNumber) ; 
-        return tempMembers ; 
-    }
-    
 }
