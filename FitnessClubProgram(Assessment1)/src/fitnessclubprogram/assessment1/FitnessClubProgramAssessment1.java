1086205d6769a66cef90e1af93c3b4dd797355fd
linke <linke@Link-Asus>
2024-03-22 11:36:25 +1000

    Cleaned up

--- FitnessClubProgram(Assessment1)/src/fitnessclubprogram/assessment1/FitnessClubProgramAssessment1.java
+++ FitnessClubProgram(Assessment1)/src/fitnessclubprogram/assessment1/FitnessClubProgramAssessment1.java
@@ -5,13 +5,7 @@ import java.util.Scanner;
 /**
  * @author linke
  */
-public class FitnessClubProgramAssessment1 {
-    static TCPClient tcpClient = new TCPClient() ; 
-    static TCPServer tcpServer = new TCPServer() ; 
-    static UDPClient udpClient = new UDPClient() ; 
-    static UDPServer udpServer = new UDPServer() ; 
-    //Member members = new Member() ; 
-    
+public class FitnessClubProgramAssessment1 {    
     public static void main(String[] args) {
         //What I am doing here, for the beginning at least is for testing choose the testing parameters via this one program 
         //The actual program will handle this differently but making it hacky atm. I think it has to be different .jar -- 
@@ -32,28 +26,19 @@ public class FitnessClubProgramAssessment1 {
         
         switch (instanceForTest) {
             case "1":
-                //TCP Client 
-                //Customer front end that gathers the information for member(s) and send it to the TCP server for adding to persistance 
-                
-                tcpClient.socketConnection() ; 
+                TCPClient.socketConnection() ; 
                 break;
             case "2":
-                //TCP Server 
-                //Receives request from TCP Client(s) and adds their entries to the persisting file 
-                tcpServer.serverMain() ; 
+                TCPServer.serverMain() ; 
                 break;
             case "3":
-                //UDP Client 
-                //Manager front end to acces records of member object 
-                
+                UDPClient.run(); ; 
                 break;
             case "4":
-                //UDP Server 
-                //Receive manager request and send back member object to be displayed through UDP Client class 
-                
+                UDPServer.run() ; 
                 break;
             default:
-                //Ignore, not gonna be invoked as not necessary 
+                System.out.println("How did you break this?") ; 
                 break;
         }
         
