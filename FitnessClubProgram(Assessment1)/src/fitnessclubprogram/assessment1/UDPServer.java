0f1f50e909c5cdfe7f19434f0da7209cdca15897
linke <linke@Link-Asus>
2024-03-22 23:43:51 +1000

    Adding comments

--- FitnessClubProgram(Assessment1)/src/fitnessclubprogram/assessment1/UDPServer.java
+++ FitnessClubProgram(Assessment1)/src/fitnessclubprogram/assessment1/UDPServer.java
@@ -9,7 +9,6 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * @author linke
@@ -18,6 +17,7 @@ public class UDPServer {
     
     //Most of the code borrowed from the tute, but adapted to fit the member details sending requirement 
     public static void run(){ 
+        //Creating the socket and creating an ArrayList<> of the deserialised member objects from file 
     	DatagramSocket aSocket = null;
         ArrayList<Member> completeList = writeToList() ; 
         if (completeList.isEmpty()) { 
@@ -25,16 +25,21 @@ public class UDPServer {
             return ; 
         }
         
+        //Send and receive from the server while it is active 
         try{
-            aSocket = new DatagramSocket(2264);
+            aSocket = new DatagramSocket(2264) ;
+            byte[] buffer = new byte[1000] ;
             
-            byte[] buffer = new byte[1000];
+            //While the connection is active, do this chunk. Will forever iterate but frankly it doesn't need to change as it works, I imagine the fix might be to send a message from the 
+            //client the server can then read as an END message. Probs a string or null object and if it errors just catch it and have the block say "End of stream" or something 
             while(true){
+                //Create the packet to prepare for sending. Catch the request and get the port, and address from it as can't hard code these in, have to be dynamic 
                 DatagramPacket request = new DatagramPacket(buffer, buffer.length);
                 aSocket.receive(request);
                 byte[] currentObject ; 
                 int length ; 
                 
+                //This is iterating over the array and sending the data one packet at a time 
                 for (int i = 0; i < completeList.size(); i++) { 
                     currentObject = serialiseObject(completeList.get(i)) ; 
                     length = currentObject.length ; 
@@ -47,6 +52,7 @@ public class UDPServer {
         }finally {if(aSocket != null) aSocket.close();}
     }
     
+    //Get the memberlistObject file and deserialise it to ArrayList<> and then send to the main method 
     private static ArrayList<Member> writeToList() { 
         ArrayList<Member> memberList = new ArrayList<>() ; 
         String fileName = "memberlistObject" ; 
@@ -72,6 +78,7 @@ public class UDPServer {
     }
     
     //Source for this: https://stackoverflow.com/questions/3736058/java-object-to-byte-and-byte-to-object-converter-for-tokyo-cabinet
+    //As far as I can explain, creates a byte array which is then initialised with the object output which the then writes the member object to be sent back to the main method as a BYTE[]
     private static byte[] serialiseObject(Member member) throws IOException{ 
         ByteArrayOutputStream out = new ByteArrayOutputStream() ; 
         ObjectOutputStream os = new ObjectOutputStream(out) ; 
@@ -80,7 +87,7 @@ public class UDPServer {
         return out.toByteArray() ; 
     } 
     
-    //To be removed but used in the meantime to check if the ArrayList<> is being populated correctly 
+    //Just an extra bit of output for the server to log, it just shows the user that the Array is populating correctly 
     private static void debugPrint(ArrayList<Member> memberList) { 
         for (int i = 0; i < memberList.size(); i++) { 
             System.out.println("Current object: " + i + " " + memberList.get(i).toString()) ; 
