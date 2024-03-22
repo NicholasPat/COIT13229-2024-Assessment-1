81e5e41ee5d8f10f3aa1ed15325ee8833437ed37
linke <linke@Link-Asus>
2024-03-22 23:43:32 +1000

    Fixed issue with the stream not sending more than one. Started adding comments

--- FitnessClubProgram(Assessment1)/src/fitnessclubprogram/assessment1/TCPClient.java
+++ FitnessClubProgram(Assessment1)/src/fitnessclubprogram/assessment1/TCPClient.java
@@ -12,24 +12,31 @@ import java.util.Scanner;
  */
 public class TCPClient {
     
-    //Will become main 
-    //THIS IS IMPORTANT TO FIX 
+    //Main class, get the members from the user and send the data to the server each time 
     //public static void main(String args[]) { 
     public static void socketConnection() { 
         Socket s = null ; 
         try { 
-            int serverPort = 8864 ; 
-            s = new Socket("localhost", serverPort) ; 
-            ObjectOutputStream out ; 
-            out = new ObjectOutputStream(s.getOutputStream()) ; 
-            
-            //For some reason, the stream isn't sending more than one object per ObjectOutputStream. Dunno how to fix so address later. For now, can only add 1 member at a time 
-            
-            Member memberToSend ; 
+            //Definitions that won't change 
+            final int serverPort = 8864 ; 
             int count = getMemberCount() ; 
+            final String hostName = "localhost" ; 
+            
+            //These steps are here as it seems the stream cannot be used more than once so essentially recreating it for the new member object to be sent 
             while (count >0) { 
+                //Creating the socket and Stream used 
+                s = new Socket(hostName, serverPort) ; 
+                ObjectOutputStream out ; 
+                out = new ObjectOutputStream(s.getOutputStream()) ; 
+                
+                //RESOLVED: For some reason, the stream isn't sending more than one object per ObjectOutputStream. Dunno how to fix so address later. For now, can only add 1 member at a time 
+                
+                //Create the member object to send to the server then send 
+                Member memberToSend ; 
                 memberToSend = getMemberDetails() ; 
                 out.writeObject(memberToSend) ; 
+                
+                //Output and count down until 0 then stop 
                 System.out.println("Sending data to the server...........") ; 
                 System.out.println(memberToSend.getFirstName() + ":" + memberToSend.getLastName() + ":" + memberToSend.getAddress() + ":" + memberToSend.getPhoneNumber()) ; 
                 System.out.println("____________________") ; 
@@ -41,6 +48,7 @@ public class TCPClient {
         }finally {if(s!=null) try {s.close();}catch (IOException e){System.out.println("close:"+e.getMessage());}}
     }
     
+    //Ask user for count of members to add 
     private static int getMemberCount() { 
         int userCount = 0 ; String number ; 
         while (userCount == 0) { 
@@ -56,6 +64,8 @@ public class TCPClient {
         return userCount ; 
     }
     
+    //Simply asking the user for a count and then sending it back to the main method and dictates how many times to iterate and ask for member objects 
+    //Don't know why I split this into this method instead of making a bulk method but it shows what this step is doing 
     private static String queryUserCount() { 
         String count ; 
         Scanner input = new Scanner(System.in) ; 
@@ -105,21 +115,15 @@ public class TCPClient {
     }
     
     //Simply, because the text of the text file is split in a specific way, error handle using that identifying string. It is incredibly unlikely to be written, but just making sure 
+    //as if I don't handle this it will break the program when the server attempts to read it 
     private static boolean memberDetailsCheck(String text) { 
         boolean result = text.contains("--__--") ; 
         if (result) { 
-            System.out.println("Invalid String '--__--' detected. Please input again without this") ; 
+            System.out.println("""
+                               Invalid String '--__--' detected. Please input again without this 
+                               Reason: Because '--__--' is used to split the text when writing to file""") ; 
             return false ; 
         }
         return true ; 
     }
-    
 }
-
-/** 
-* Pseudocode! 
-* Ask for number of members the user wishes to add 
-* Ask for member details and create the member object 
-* Send the data to the "server" and then output customer data 
-* Send out server response "save data for member number 1" 
-*/
